9bd6fcc171664f88217fc6b94172c961b5144a1f
Romain Alcon <romainalcon@hotmail.com>
2021-06-16 01:56:00 +0200

    feat: fisrt link open!

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'assets/class/Database.php';
+$db = Database::getInstance();
+
+$domain = $_SERVER['HTTP_HOST'];
+$link = $_GET['link'];
+
+$statement = $db->prepare("SELECT links.url, links.nbused FROM links LEFT JOIN domains ON domains.id = links.domainsid WHERE domains.name = ? AND links.name = ?");
+$statement->execute([$domain, $link]);
+if ($statement->rowCount() >= 1) {
+    $result = $statement->fetch();
+    $response = $db->prepare("UPDATE links SET nbused = ? WHERE name = ?");
+    $response->execute([$result['nbused'] + 1, $link]);
+    header('Location: '.$result['url']);
+} else {
+    ?>
+    Le lien que vous rechercher n'existe pas !
+    <?php
+}
--- login.php
+++ login.php
@@ -10,8 +10,54 @@ $db = Database::getInstance();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Connexion - Shortened</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            display: flex;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background: #f5f5f5;
+        }
+        .form-signin {
+            width: 100%;
+            max-width: 330px;
+            padding: 15px;
+            margin: auto;
+        }
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+        .form-signin input[type='text'] {
+            margin-bottom: -1px;
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+        .form-signin input[type='password'] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+    </style>
 </head>
-<body>
+<body class="text-center">
+    <main class="form-signin">
+        <form action="">
+            <h1 class="mb-4 h2 fw-normal">Shortened</h1>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="usernameForm" name="username" placeholder="johndoe@gmail.com">
+                <label for="usernameForm">Nom d'utilisateur</label>
+            </div>
+            <div class="form-floating mb-4">
+                <input type="password" class="form-control" id="passwordForm" name="password" placeholder="ChEvrEd0usse">
+                <label for="passwordForm">Mot de passe</label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary mb-4" type="submit">Connexion</button>
+            <a href="#" class="mb-3 text-muted">Mot de passe oublié</a>
+        </form>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
